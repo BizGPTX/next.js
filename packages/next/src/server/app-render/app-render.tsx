4738563e6bb933e7c13b266bb9f6404fa271027b
Hendrik Liebau <mail@hendrik-liebau.de>
2025-08-20 21:15:48 +0200

    Clean up controllers when debug channel is closed

--- packages/next/src/server/app-render/app-render.tsx
+++ packages/next/src/server/app-render/app-render.tsx
@@ -2618,7 +2618,11 @@ async function renderToStream(
 
 function createDebugChannel(
   sendReactDebugChunk:
-    | ((chunk: Uint8Array, htmlRequestId: string, requestId: string) => void)
+    | ((
+        chunk: Uint8Array | null,
+        htmlRequestId: string,
+        requestId: string
+      ) => void)
     | undefined,
   htmlRequestId: string,
   requestId: string,
@@ -2645,6 +2649,8 @@ function createDebugChannel(
         sendReactDebugChunk(chunk, htmlRequestId, requestId)
       },
       close() {
+        // A null chunk signals to the client that no more chunks will be sent.
+        sendReactDebugChunk(null, htmlRequestId, requestId)
         readableController?.close()
       },
     }),
